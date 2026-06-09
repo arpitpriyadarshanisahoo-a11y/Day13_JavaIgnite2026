1e309be98da25da40bae63b1e444a39258c18ef0
rite2harini <rite2harini@gmail.com>
2026-06-09 13:20:08 +0530

    Create TaskScheduler with three independent threads
    Implement a TaskScheduler with three independent threads that print numbers as specified.

---
+++ Multithreading/TaskScheduler.java
@@ -0,0 +1,17 @@
+/*
+Create a program with 3 threads:
+
+Thread 1 → prints numbers 1–5
+Thread 2 → prints even numbers 2–10
+Thread 3 → prints odd numbers 1–9
+Task:
+Ensure all threads run independently
+Print thread name with each output
+Hint:
+
+Think:
+
+Each thread = separate class OR Runnable
+Use run() method logic
+Focus on parallel execution behavior
+*/
